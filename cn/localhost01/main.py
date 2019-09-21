8ac76c6c08575ef86b42d30be69ccf0dacad58c2
niuben <820713556@qq.com>
2019-09-21 17:02:55 +0800

    第四次提交，实现单元格文本格式转化为数字

--- cn/localhost01/main.py
+++ cn/localhost01/main.py
@@ -91,63 +91,88 @@ import xlrd
 import xlwt
 from datetime import date, datetime
 from xlutils.copy import copy
+from xlrd import XL_CELL_NUMBER
+
+from openpyxl.styles import numbers
+
 
 
 def read_excel():
     # 打开文件
-    workbook = xlrd.open_workbook(r'D:\test5.xlsx')
+    # workbook = xlrd.open_workbook(r'D:\test5.xlsx')
+    workbook = xlrd.open_workbook(r'D:\test5.xls')
     new_workbook = copy(workbook)  # 将xlrd对象拷贝转化为xlwt对象
     new_worksheet = new_workbook.get_sheet(0)  # 获取转化后工作簿中的第一个表格
 
-    # wbook = copy(workbook)  # wbook即为xlwt.WorkBook对象
-    # wsheet = wbook.get_sheet(0)  # 通过get_sheet()获取的sheet有write()方法
-    # wr = xlwt.open_workbook(r'D:\test5.xlsx')
-    # workbook = load_workbook(r'D:\test5.xlsx')
-
+    # 预定一个格式
+    style = xlwt.XFStyle()
+    style.number_format = numbers.FORMAT_NUMBER
     # 根据sheet索引或者名称获取sheet内容
     sheet2 = workbook.sheet_by_index(0)  # sheet索引从0开始
-    # sheet2_wr = wr.sheet_by_index(0)  # sheet索引从0开始
-    # sheet2 = workbook.sheet_by_name('sheet2')
 
     # sheet的名称，行数，列数
-    print(sheet2.name, sheet2.nrows, sheet2.ncols)
+    # print(sheet2.name, sheet2.nrows, sheet2.ncols)
 
     # 获取整行和整列的值（数组）
-    rows = sheet2.row_values(3)  # 获取第四行内容
-    cols = sheet2.col_values(2)  # 获取第三列内容
-    cols = sheet2.col_values(8)  # 获取第8列内容
-    print(rows)
-    print(cols)
+    # rows = sheet2.row_values(3)  # 获取第四行内容
+    # cols = sheet2.col_values(2)  # 获取第三列内容
+    # cols = sheet2.col_values(8)  # 获取第8列内容
+    # print(rows)
+    # print(cols)
 
 
     # 获取单元格内容
     cols = sheet2.col_values(8)  # 获取第8列内容
+    print(sheet2.cell(7, 7).ctype)
+    # xlrd.xlnumber_as_tuple(sheet2.cell_value(7, 4), 2)
+    # xlrd.XL_CELL_NUMBER(sheet2.cell_value(7, 4))
 
-    # print(sheet2.cell(0, 0))
-    # print(sheet2.col_values(7)[4])
+    # sheet2.cell.ctype == XL_CELL_NUMBER
+
+    # print(sheet2.cell_value(10, 7))
+    # new_worksheet.write(10, 7, sheet2.cell_value(10, 7))
+
+    # print(sheet2.cell_type(7, 7))
+    # print(sheet2.cell_type(12, 7))
+
+    # sheet2.cell['H'].number_format = numbers.FORMAT_GENERAL
+    # sheet2.cell_value(10, 7).number_format = numbers.FORMAT_NUMBER
+
+    # sheet2.cell_value(10, 7).number_format = numbers.FORMAT_NUMBER
+    print(sheet2.cell_type(13, 7))
+    sheet2.cell(13, 7).number_format = numbers.NumberFormat
+
+    # sheet2.cell(13, 7).number_format = numbers
+
+    sheet2.cell(12, 8).number_format = numbers.NumberFormat
+    new_write = sheet2.col_values(8)[12]
+    print(new_write)
+    # new_write.number_format = numbers
+    new_write = int(new_write)
+    new_worksheet.write(12, 8, new_write, style)
+    # new_worksheet.write(4, 8, new_write, style)
+    print(sheet2.col_values(12, 8))
+    new_workbook.save(r'D:\test5.xls')  # 保存工作簿
+    # ord(sheet2.cell_value(10, 7))
+    # new_workbook.save(r'D:\test5.xls')  # 保存工作簿
+    # print(sheet2.cell_type(7, 7))
+    # print(xlrd.XL_CELL_NUMBER('12'))
+    # print(sheet2.col_values(7)[4].ctype)
 
     # 用for循环的方式连续向单元格中写入内容
-    for i in range(7, 9):
-        for j in range(4, 359):
-            # print(sheet2.col_values(7)[4].replace('CSC-J', ''))
-            # print(sheet2.col_values(7)[4].replace('CSC-J', ''))
-            write = sheet2.col_values(i)[j].replace('CSC-J', '')
-            new_worksheet.write(j, i, write)
-            new_workbook.save(r'D:\test5.xls')  # 保存工作簿
-            print(sheet2.col_values(i)[j])
-            print("xls格式表格【追加】写入数据成功！")
+    # for i in range(7, 9):
+    #     for j in range(4, 359):
+    #         write = sheet2.col_values(i)[j].replace('CSC-J', '')
+    #         new_worksheet.write(j, i, write)
+    #         new_workbook.save(r'D:\test5.xls')  # 保存工作簿
+    #         print(sheet2.col_values(i)[j])
+    #         print("xls格式表格【追加】写入数据成功！")
 
 
 
 
 
 
-    # print(sheet2.cell(1, 0).value.encode('utf-8'))
-    # print(sheet2.cell_value(1, 0)# workbook.save(r'D:\test5.xlsx') .encode('utf-8'))
-    # print(sheet2.row(1)[0].value.encode('utf-8'))
-
-    # 获取单元格内容的数据类型
-    # print(sheet2.cell(1, 0).ctype)
 
 
 
